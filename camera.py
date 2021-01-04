058587d90fa96490a02a6822e18b66c371f7d000
usamamirza0 <76936167+usamamirza0@users.noreply.github.com>
2021-01-04 12:03:59 +0500

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,21 @@
+from picamera import PiCamera
+from time import sleep
+import os
+
+if os.path.isfile("image0.jpg"):
+	print("Images already captured")
+
+else:
+	camera = PiCamera()
+	camera.resolution = (1024, 1024)
+
+	for i in range(10):
+		imagename = "image" + str(i) + ".jpg"
+		print("Capturing " + imagename)
+		camera.start_preview()
+		sleep(5)
+		camera.capture(imagename)
+		camera.stop_preview()
+		sleep(5)
+
+	print(subprocess.call("./test.sh"))
---
+++ delete_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm image0.jpg
+rm image1.jpg
+rm image2.jpg
+rm image3.jpg
+rm image4.jpg
+rm image5.jpg
+rm image6.jpg
+rm image7.jpg
+rm image8.jpg
+rm image9.jpg
